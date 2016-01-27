a906b1586035af45239920978ef4a92abc7ccada
Prashantgemini73 <prashantgemini73@gmail.com>
2016-01-27 17:03:13 +0530

    Python Code

---
+++ Test.py
@@ -0,0 +1,37 @@
+#!C:/Python27/python.exeam
+
+print
+"Good!"
+phrase="\nHello Madam"
+print {'Hello Swapneel'}
+print phrase
+
+fiveto9 = 'workshop'
+print fiveto9
+
+
+a=1,2,3,4
+print a
+
+a,b,c = 1,2,3
+a,b,c = b,c,a
+print a
+print b
+print c
+
+print "\n\n"
+
+a, b = 2,3 
+b, d = b+1, a
+print a, b, d
+
+print type(24)
+
+print "\n\n"
+zipcode = 0x2132
+print type(zipcode)
+print zipcode
+
+ub=99999999999999999999999999999999999999999999999999999999999999999999999999
+print ub
+print 4**2
